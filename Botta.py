4bf47d9bf2605b06951066e5ba11cb6a2c4d99fc
Pr0xximity999 <tomsomgamer999@gmail.com>
2023-08-28 22:05:43 +0200

    Default bot made

--- Botta.py
+++ Botta.py
@@ -3,14 +3,26 @@ import discord
 import pandas as pd
 import pathlib
 
-
+client = discord.client
 class MyClient(discord.Client):
+
+    #When the bot has started
     async def on_ready(self):
         print(f'Logged on as {self.user}!')
 
+    #when there is a message sent
     async def on_message(self, message):
-        print(f'Message from {message.author}: {message.content}')
+        if(message.author.id != self.user.id):
+            print(f'Message from {message.author}: {message.content}')
 
+        if(message.author.id != self.user.id and message.content[0] == "-"):
+            channel = client.get_channel(message.channel.id)
+            await channel.send(message.content[1:])
+           
+
+        
+
+#Gets the intents and adds them to the client
 intents = discord.Intents.default()
 intents.message_content = True
 
