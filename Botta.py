e2fc510352cf0721ccac09c97032a7eb6c3312a1
Pr0xximity999 <tomsomgamer999@gmail.com>
2023-08-29 14:11:24 +0200

    bitchass buttons no work

--- Botta.py
+++ Botta.py
@@ -1,46 +1,91 @@
 #Import libraries
 import discord
-from discord import app_commands
+from discord.ext import commands
+from discord.components import Button, ButtonStyle
 import pandas as pd
 import pathlib
 
-class MyClient(discord.Client):
-    
-    #When the bot has started
-    async def on_ready(self):
-        await tree.sync(guild=discord.Object(id=1145786156185829407))
-        print(f'Logged on as {self.user}!')
-
-    #when there is a message sent
-    async def on_message(self, message):
-        if(message.author == self.user): return
-        
-        print(f'Message from {message.author}: {message.content}')
-
-        if(message.content[0] == "-"):
-            try:
-                channel = client.get_channel(message.channel.id)
-                msg = await channel.fetch_message(message.id)
-                await msg.delete()
-                await channel.send(message.content[1:])
-            except:
-                pass
-
-#Gets the intents and adds them to the client
 intents = discord.Intents.default()
 intents.message_content = True
+intents.guilds = True
 
-#Defines the client
-client = MyClient(intents=intents)
-#Sets the app commands
-tree = app_commands.CommandTree(client)
+bot = commands.Bot(command_prefix='-', intents=intents)
 
-#Sets the slash commands
-@tree.command(name="testcommand",  description="this does something", guild=discord.Object(id=1145786156185829407))
-async def embed_command(interaction, parameter1: int = 10):
-    await interaction.response.send_message("Piss")
+users = {}
+games = []
+votes = {}
+
+@bot.event
+async def on_ready():
+    print(f'Bot is ingelogd als {bot.user}')
+
+
+@bot.command()
+async def send_embed(ctx):
+    embed = discord.Embed(title="Input Form", description="Enter something:")
+    await ctx.send(
+        embed=embed,
+        components=[
+            Button(style=ButtonStyle.blurple, label="Click me!", custom_id="input_button"),
+        ],
+    )
+
+@bot.event
+async def on_button_click(interaction):
+    if interaction.custom_id == "input_button":
+        await interaction.respond(
+            type=7,  # Acknowledge the interaction
+            content="Please enter something:",
+            components=[
+                Button(style=ButtonStyle.blurple, label="Submit", custom_id="confirm_button"),
+            ],
+        )
+
+@bot.event
+async def on_button_click(interaction):
+    if interaction.custom_id == "confirm_button":
+        user_input = interaction.message.content  # Get the content of the message
+        await interaction.respond(content=f"You entered: {user_input}")
+
+
+
+@bot.command()
+async def start_vote(ctx, game_name):
+    
+
+    if game_name not in games: 
+        games.append(game_name)
+        votes[game_name] = 0
+    else: 
+        ctx.send('Dat nummer is al toegevoegt!')
+        return
+    await ctx.send(f'{game_name} is toegevoegd aan de lijst!')
+
+@bot.command()
+async def show_gamelist(ctx):
+    await ctx.send(games)
+
+@bot.command()
+async def vote(ctx, game_name):
+    if game_name in games:
+        if ctx.author.id not in votes:
+            votes[ctx.author.id] = game_name
+            await ctx.send(f'Je hebt gestemd voor {game_name}!')
+        else:
+            await ctx.send('Je hebt al gestemd!')
+    else:
+        await ctx.send('Dit spel staat niet in de lijst!')
+
+@bot.command()
+async def calculate_winner(ctx):
+    for vote in votes.values():
+        if vote in games:
+            games[vote] += 1
+
+    winner = max(games, key=games.get)
+    await ctx.send(f'Het winnende spel is {winner}!')
 
 #retrieves the api key from the csv and runs the client
 secrets = pd.read_csv(f'{pathlib.Path(__file__).parent.resolve()}\secrets.csv')
 apikey = secrets["APIKey"].values[0]
-client.run(apikey)
+bot.run(apikey)
