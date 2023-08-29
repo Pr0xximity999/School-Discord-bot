a2ae21f63aed18707257d6bac41f4b5aab0eccfa
Pr0xximity999 <tomsomgamer999@gmail.com>
2023-08-29 11:04:12 +0200

    slash command

--- Botta.py
+++ Botta.py
@@ -1,21 +1,23 @@
 #Import libraries
 import discord
+from discord import app_commands
 import pandas as pd
 import pathlib
 
-client = discord.client
 class MyClient(discord.Client):
-
+    
     #When the bot has started
     async def on_ready(self):
+        await tree.sync(guild=discord.Object(id=1145786156185829407))
         print(f'Logged on as {self.user}!')
 
     #when there is a message sent
     async def on_message(self, message):
-        if(message.author.id != self.user.id):
-            print(f'Message from {message.author}: {message.content}')
+        if(message.author == self.user): return
+        
+        print(f'Message from {message.author}: {message.content}')
 
-        if(message.author.id != self.user.id and message.content[0] == "-"):
+        if(message.content[0] == "-"):
             try:
                 channel = client.get_channel(message.channel.id)
                 msg = await channel.fetch_message(message.id)
@@ -24,14 +26,19 @@ class MyClient(discord.Client):
             except:
                 pass
 
-
-        
-
 #Gets the intents and adds them to the client
 intents = discord.Intents.default()
 intents.message_content = True
 
+#Defines the client
 client = MyClient(intents=intents)
+#Sets the app commands
+tree = app_commands.CommandTree(client)
+
+#Sets the slash commands
+@tree.command(name="testcommand",  description="this does something", guild=discord.Object(id=1145786156185829407))
+async def embed_command(interaction, parameter1: int = 10):
+    await interaction.response.send_message("Piss")
 
 #retrieves the api key from the csv and runs the client
 secrets = pd.read_csv(f'{pathlib.Path(__file__).parent.resolve()}\secrets.csv')
