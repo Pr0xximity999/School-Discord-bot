ca3f7146a295b6ec0fcab0b4655feac04e7ca123
Pr0xximity999 <tomsomgamer999@gmail.com>
2023-08-31 11:51:07 +0200

    added stages and players will get mentioned if they can add a game

--- Botta.py
+++ Botta.py
@@ -5,9 +5,17 @@ import pandas as pd
 import pathlib
 import re
 
+currUser = ""
 voteStarted = False
 hostUser = ""
+userIndex = -1
+userDone = True
 users = {}
+gameStage = {
+    "addusers" : True,
+    "addgames" : False,
+    "votegames": False
+}
 
 class MyClient(discord.Client):
     #When the bot has started
@@ -16,24 +24,53 @@ class MyClient(discord.Client):
         print(f'Logged on as {self.user}')
 
     #when there is a message sent
-    async def on_message(self, message):
-        if(message.author == self.user): return
-        if(message.author.id == hostUser):
-            channel = client.get_channel(message.channel.id)
-            if(not "@" in message.content):
-                
+    async def on_message(self, message:discord.Message):
+        global currUser, userDone, userIndex
+        if(message.author == self.user): return #If the bot has sent the message, ignore it
+        channel = client.get_channel(message.channel.id) #Sets the channel for replying
+
+        #If the game stage is to add the users
+        if(gameStage["addusers"]):
+            if(message.author.id != hostUser): return #If the user is anything other than the host
+
+            #If theres no mention in the message
+            if(not "@" in message.content):         
                 await channel.send("There's not a single user in that sentence")
                 await channel.send("dumbass")
+
+            #Go trough the message to add all the(unique users) users
             for _, user in enumerate(re.finditer(r"<.{19}>", message.content)):
                 if user.group() not in users.keys():
                     if(not user.group()): continue 
                     users[user.group()] = {"game1": "",
-                                                 "game2": "",
-                                                 "game3": ""}
-            if(users == {}): return
+                                                "game2": "",
+                                                "game3": ""}
+            if(users == {}): return #If no users where found, ingore
+
+            #Display the participating users and explains the rules
             await channel.send("Players who participate:")
             for user in users.keys():
                 await channel.send(user)
+
+            await channel.send("Now all players will add their games")
+            await channel.send("The games have to be unique, so no doubles")
+
+            #Switch gamestage
+            gameStage["addusers"] = False
+            gameStage["addgames"] = True
+            
+        #If the game stake is to add the games
+        if(gameStage["addgames"]):
+            if(userDone):
+                userIndex += 1
+                currUser = list(users.keys())[userIndex]
+                await channel.send(f"{currUser}, please add your 3 games")
+                userDone = False
+                return
+            if(message.author.id != currUser): return
+                
+
+
 
 
 #Gets the intents and adds them to the client
